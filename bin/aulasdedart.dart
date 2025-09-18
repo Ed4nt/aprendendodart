ca326b4a219a8af4b6f6cbd6681ac0d1a7cc9634
Edelson Bessa <edelsonbessa@gmail.com>
2025-09-18 10:18:24 -0400

    Iniciando aulas

---
+++ bin/aulasdedart.dart
@@ -0,0 +1,5 @@
+import 'package:aulasdedart/aulasdedart.dart' as aulasdedart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aulasdedart.calculate()}!');
+}
---
+++ test/aulasdedart_test.dart
@@ -0,0 +1,8 @@
+import 'package:aulasdedart/aulasdedart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
