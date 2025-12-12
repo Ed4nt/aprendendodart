2cb68d41729110ffe488115c4aa3448e9115b4b1
Edelson Bessa <edelsonbessa@gmail.com>
2025-12-12 18:35:52 -0300

    Aula de sets

---
+++ lib/colecoes/sets.dart
@@ -0,0 +1,8 @@
+// É uma lista que não possui VALORES duplicados
+
+void main () {
+  
+  Set<String> frutas = {"Maçã", "Mamão", "Pera", "Abacate", "Maçã", "Mamão"};
+
+  print("Frutas Set: $frutas"); // Frutas Set: {Maçã, Mamão, Pera, Abacate}
+}
