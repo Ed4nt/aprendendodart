76c612970e833a2b6bdadbe43e9d1ed74dda742c
Edelson Bessa <edelsonbessa@gmail.com>
2025-12-13 13:37:43 -0300

    Aula de Sets

--- lib/colecoes/sets.dart
+++ lib/colecoes/sets.dart
@@ -1,8 +1,23 @@
 // É uma lista que não possui VALORES duplicados
 
 void main () {
-  
-  Set<String> frutas = {"Maçã", "Mamão", "Pera", "Abacate", "Maçã", "Mamão"};
-
+  // O próprio nome do Set serve como chave
+  Set<String> frutas = {"Maçã", "Mamão", "Pera", "Abacate", "Maçã", "Mamão"}; // Os valores repetidos serão ignorados
   print("Frutas Set: $frutas"); // Frutas Set: {Maçã, Mamão, Pera, Abacate}
+
+  final fruits = {"Abacaxi", "Melão"};
+  print(fruits);
+  fruits.add("Avocado");
+  print(fruits);
+  fruits.remove("Abacaxi");
+  print(fruits);
+  print(fruits.contains("Abóbora"));
+
+  List<int> numeros = [1,1,1,2,2,2,3,3,3,];
+  numeros = numeros.toSet().toList();
+  print(numeros);
+
+  for (var fruta in fruits) {
+    print("Fruta: $fruta");
+  }
 }
